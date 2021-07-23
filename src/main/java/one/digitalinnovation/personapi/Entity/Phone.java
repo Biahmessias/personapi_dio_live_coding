5495fcbcf0cb7f78828cc2b14e0bec26a074ef04
Beatriz messias <biahmessias@gmail.com>
2021-07-23 15:00:31 -0300

    initial commit

---
+++ src/main/java/one/digitalinnovation/personapi/Entity/Phone.java
@@ -0,0 +1,22 @@
+package one.digitalinnovation.personapi.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Phone {
+
+    private Long id;
+
+    private PhoneType type;
+
+    private String number;
+
+
+}
